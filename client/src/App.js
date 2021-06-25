cc232420b1a9a63c14b0fe57635c7f7651d4c2f2
Hannachi-Wafa <80890958+Hannachi-Wafa@users.noreply.github.com>
2021-06-25 23:20:28 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,6 @@ function App() {
       <Route exact path='/Detailsproduct' component={Detailsproduct}></Route>
       <PrivateRoute exact path='/profileAdmin' component={ProfileAdmin}></PrivateRoute>
       <PrivateRouteUser exact path='/profile' component={Profile}></PrivateRouteUser>
-
       </Switch>
     </>
   );
